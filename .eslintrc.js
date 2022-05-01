e6effb84f1e0cfe68db3b1a7ba55673387d07c1c
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-02 00:47:27 +0300

    Tune esling config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -13,4 +19,64 @@ module.exports = {
       },
     },
   ],
-};
+  rules: {
+    semi: 'off',
+    'no-console': 'error',
+    'no-undef': 'off',
+    'no-unneeded-ternary': 'error',
+    'no-nested-ternary': 'error',
+    curly: 'error',
+    'no-underscore-dangle': 'error',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'var', 'case', 'default'],
+        next: '*',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['return', 'export'],
+      },
+    ],
+    'import/no-cycle': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-unresolved': 'off',
+    'import/namespace': 'off',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: 'react**',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@app/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'index'],
+          ['sibling'],
+          ['type'],
+          ['object'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        pathGroupsExcludedImportTypes: [],
+      },
+    ],
+    'react/jsx-newline': 'error',
+  },
+}
