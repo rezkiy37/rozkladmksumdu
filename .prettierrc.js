daaca0234e712cf1965bb090b9b1fd07d375b5c8
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-02 00:46:46 +0300

    Tune ts prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
   trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  bracketSameLine: false,
+  singleAttributePerLine: true,
 };
