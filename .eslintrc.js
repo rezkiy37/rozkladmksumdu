e79c0707acb7c9baec62c89ca88fc4a02545a064
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-02 21:59:32 +0300

    Improve eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,11 @@ module.exports = {
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: ['@typescript-eslint', 'prettier'],
   overrides: [
     {
