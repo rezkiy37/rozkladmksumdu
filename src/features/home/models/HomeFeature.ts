7972f09e018ec0a2aab5939e70d3a5614a1c572e
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-13 22:00:25 +0300

    Integrate views, actions in HomeFeatureModel

--- src/features/home/models/HomeFeature.ts
+++ src/features/home/models/HomeFeature.ts
@@ -1,22 +1,45 @@
-import { Instance, SnapshotOut, types } from 'mobx-state-tree'
+import { Instance, types } from 'mobx-state-tree'
 
 import { ApiModel, initialState as apiInitialState } from '@app/models/Api'
-import { GroupModel } from '@app/models/Group'
-import { ScheduleChangeModel } from '@app/models/ScheduleChange'
+import { Group, GroupModel } from '@app/models/Group'
+import { ScheduleChange, ScheduleChangeModel } from '@app/models/ScheduleChange'
 
 import { ModelName } from './ModelName'
 
-export const HomeFeatureModel = types.model(ModelName.HomeFeature, {
-  api: ApiModel,
-  groups: types.array(GroupModel),
-  scheduleChanges: types.array(ScheduleChangeModel),
-})
+export const HomeFeatureModel = types
+  .model(ModelName.HomeFeature, {
+    api: ApiModel,
+    groups: types.array(GroupModel),
+    scheduleChanges: types.array(ScheduleChangeModel),
+  })
+  .views(self => ({
+    get scheduleChangesAmount(): string {
+      return self.scheduleChanges.length.toString()
+    },
+  }))
+  .actions(self => {
+    function setGroups(groups: Group[]) {
+      self.groups.replace(groups)
+    }
+
+    function clearGroups() {
+      self.groups.clear()
+    }
+
+    function setScheduleChanges(scheduleChanges: ScheduleChange[]) {
+      self.scheduleChanges.replace(scheduleChanges)
+    }
+
+    function clearScheduleChanges() {
+      self.scheduleChanges.clear()
+    }
+
+    return { setGroups, clearGroups, setScheduleChanges, clearScheduleChanges }
+  })
 
 export type HomeFeature = Instance<typeof HomeFeatureModel>
 
-type HomeFeatureProps = SnapshotOut<typeof HomeFeatureModel>
-
-export const initialState: HomeFeatureProps = HomeFeatureModel.create({
+export const initialState = HomeFeatureModel.create({
   api: apiInitialState,
   groups: [],
   scheduleChanges: [],
