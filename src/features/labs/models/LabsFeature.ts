10b173960bfb1b05b185b518a6b5ab56a9b1e9f8
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-13 22:00:46 +0300

    Integrate views, actions in LabsFeatureModel

--- src/features/labs/models/LabsFeature.ts
+++ src/features/labs/models/LabsFeature.ts
@@ -1,20 +1,35 @@
-import { Instance, SnapshotOut, types } from 'mobx-state-tree'
+import { Instance, types } from 'mobx-state-tree'
 
 import { ApiModel, initialState as apiInitialState } from '@app/models/Api'
-import { LabModel } from '@app/models/Lab'
+import { Lab, LabModel } from '@app/models/Lab'
 
 import { ModelName } from './ModelName'
 
-export const LabsFeatureModel = types.model(ModelName.LabsFeature, {
-  api: ApiModel,
-  labs: types.array(LabModel),
-})
+export const LabsFeatureModel = types
+  .model(ModelName.LabsFeature, {
+    api: ApiModel,
+    labs: types.array(LabModel),
+  })
+  .views(self => ({
+    get labsAmount(): string {
+      return self.labs.length.toString()
+    },
+  }))
+  .actions(self => {
+    function setLabs(labs: Lab[]) {
+      self.labs.replace(labs)
+    }
+
+    function clearLabs() {
+      self.labs.clear()
+    }
+
+    return { setLabs, clearLabs }
+  })
 
 export type LabsFeature = Instance<typeof LabsFeatureModel>
 
-type LabsFeatureProps = SnapshotOut<typeof LabsFeatureModel>
-
-export const initialState: LabsFeatureProps = LabsFeatureModel.create({
+export const initialState = LabsFeatureModel.create({
   api: apiInitialState,
   labs: [],
 })
