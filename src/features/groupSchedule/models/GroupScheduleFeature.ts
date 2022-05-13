0cc1e9cca0850baed15253d0b2671bcf0650edd9
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-13 22:00:38 +0300

    Integrate actions in GroupScheduleFeatureModel

--- src/features/groupSchedule/models/GroupScheduleFeature.ts
+++ src/features/groupSchedule/models/GroupScheduleFeature.ts
@@ -1,24 +1,41 @@
-import { Instance, SnapshotOut, types } from 'mobx-state-tree'
+import { Instance, types } from 'mobx-state-tree'
 
 import { ApiModel, initialState as apiInitialState } from '@app/models/Api'
-import { ScheduleModel } from '@app/models/Schedule'
+import { Group, GroupModel } from '@app/models/Group'
+import { Schedule, ScheduleModel } from '@app/models/Schedule'
 
 import { ModelName } from './ModelName'
 
-export const GroupScheduleFeatureModel = types.model(
-  ModelName.GroupScheduleFeature,
-  {
+export const GroupScheduleFeatureModel = types
+  .model(ModelName.GroupScheduleFeature, {
     api: ApiModel,
+    selectedGroup: types.maybeNull(GroupModel),
     groupSchedules: types.array(ScheduleModel),
-  },
-)
+  })
+  .actions(self => {
+    function setGroup(group: Group) {
+      self.selectedGroup = group
+    }
+
+    function clearGroup() {
+      self.selectedGroup = null
+    }
+
+    function setGroupSchedules(groupSchedules: Schedule[]) {
+      self.groupSchedules.replace(groupSchedules)
+    }
+
+    function clearGroupSchedules() {
+      self.groupSchedules.clear()
+    }
+
+    return { setGroup, clearGroup, setGroupSchedules, clearGroupSchedules }
+  })
 
 export type GroupScheduleFeature = Instance<typeof GroupScheduleFeatureModel>
 
-type GroupScheduleFeatureProps = SnapshotOut<typeof GroupScheduleFeatureModel>
-
-export const initialState: GroupScheduleFeatureProps =
-  GroupScheduleFeatureModel.create({
-    api: apiInitialState,
-    groupSchedules: [],
-  })
+export const initialState = GroupScheduleFeatureModel.create({
+  api: apiInitialState,
+  selectedGroup: null,
+  groupSchedules: [],
+})
