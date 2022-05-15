b8ac2f37cc8dae58236974a6c8661a6e61ed04de
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-16 00:20:13 +0300

    Intefrate reference selected group in GroupScheduleFeatureModel

--- src/features/groupSchedule/models/GroupScheduleFeature.ts
+++ src/features/groupSchedule/models/GroupScheduleFeature.ts
@@ -1,15 +1,32 @@
-import { Instance, types } from 'mobx-state-tree'
+import { flow, Instance, types, getRoot } from 'mobx-state-tree'
 
 import { ApiModel, initialState as apiInitialState } from '@app/models/Api'
 import { Group, GroupModel } from '@app/models/Group'
 import { Schedule, ScheduleModel } from '@app/models/Schedule'
+import { Store } from '@app/models/Store'
+import { groupScheduleApiService } from '@app/services/api'
+import { convertToSchedule } from '@app/utils/converters'
 
 import { ModelName } from './ModelName'
+
+const SelectedGroupReference = types.maybeNull(
+  types.reference(GroupModel, {
+    get(id, parent): any {
+      const store = getRoot(parent)
+
+      return (store as Store).home.groups.find(group => group.id === id) ?? null
+    },
+    set(group) {
+      return group.id
+    },
+  }),
+)
 
 export const GroupScheduleFeatureModel = types
   .model(ModelName.GroupScheduleFeature, {
     api: ApiModel,
-    selectedGroup: types.maybeNull(GroupModel),
+    selectedGroup: SelectedGroupReference,
+    // TODO: Rework to days and day schedule
     groupSchedules: types.array(ScheduleModel),
   })
   .actions(self => {
@@ -29,11 +46,33 @@ export const GroupScheduleFeatureModel = types
       self.groupSchedules.clear()
     }
 
+    const uploadGroupSchedules = flow(function* () {
+      self.api.startLoading()
+
+      try {
+        if (!self.selectedGroup?.id) {
+          throw new Error('There is not selected group')
+        }
+
+        const groupSchedules: Schedule[] =
+          yield groupScheduleApiService.getGroupSchedule(self.selectedGroup.id)
+
+        setGroupSchedules(groupSchedules.map(convertToSchedule))
+      } catch (e) {
+        const errorMessage = String(e)
+
+        self.api.setErrorMessage(errorMessage)
+      } finally {
+        self.api.finishLoading()
+      }
+    })
+
     return {
       setSelectedGroup,
       clearSelectedGroup,
       setGroupSchedules,
       clearGroupSchedules,
+      uploadGroupSchedules,
     }
   })
 
