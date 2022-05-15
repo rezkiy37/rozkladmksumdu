2cb3df746fa62efd9b1fd664ae0db6d01fc59d98
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-16 00:20:56 +0300

    Add uploadScheduleChanges to HomeFeatureModel

--- src/features/home/models/HomeFeature.ts
+++ src/features/home/models/HomeFeature.ts
@@ -1,8 +1,10 @@
-import { Instance, types } from 'mobx-state-tree'
+import { flow, Instance, types } from 'mobx-state-tree'
 
 import { ApiModel, initialState as apiInitialState } from '@app/models/Api'
 import { Group, GroupModel } from '@app/models/Group'
 import { ScheduleChange, ScheduleChangeModel } from '@app/models/ScheduleChange'
+import { groupsApiService, scheduleChangesApiService } from '@app/services/api'
+import { convertToGroup, convertToScheduleChange } from '@app/utils/converters'
 
 import { ModelName } from './ModelName'
 
@@ -34,7 +36,47 @@ export const HomeFeatureModel = types
       self.scheduleChanges.clear()
     }
 
-    return { setGroups, clearGroups, setScheduleChanges, clearScheduleChanges }
+    const uploadGroups = flow(function* () {
+      self.api.startLoading()
+
+      try {
+        const groups: Group[] = yield groupsApiService.getGroups()
+
+        setGroups(groups.map(convertToGroup))
+      } catch (e) {
+        const errorMessage = String(e)
+
+        self.api.setErrorMessage(errorMessage)
+      } finally {
+        self.api.finishLoading()
+      }
+    })
+
+    const uploadScheduleChanges = flow(function* () {
+      self.api.startLoading()
+
+      try {
+        const scheduleChanges: ScheduleChange[] =
+          yield scheduleChangesApiService.getScheduleChanges()
+
+        setScheduleChanges(scheduleChanges.map(convertToScheduleChange))
+      } catch (e) {
+        const errorMessage = String(e)
+
+        self.api.setErrorMessage(errorMessage)
+      } finally {
+        self.api.finishLoading()
+      }
+    })
+
+    return {
+      setGroups,
+      clearGroups,
+      setScheduleChanges,
+      clearScheduleChanges,
+      uploadScheduleChanges,
+      uploadGroups,
+    }
   })
 
 export type HomeFeature = Instance<typeof HomeFeatureModel>
