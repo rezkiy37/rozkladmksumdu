0063b5997bb6fd62c08010c4af79aa64cdf9188b
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-16 00:19:45 +0300

    Add uploadLabs to LabsFeatureModel

--- src/features/labs/models/LabsFeature.ts
+++ src/features/labs/models/LabsFeature.ts
@@ -1,7 +1,9 @@
-import { Instance, types } from 'mobx-state-tree'
+import { flow, Instance, types } from 'mobx-state-tree'
 
 import { ApiModel, initialState as apiInitialState } from '@app/models/Api'
 import { Lab, LabModel } from '@app/models/Lab'
+import { scheduleLabsApiService } from '@app/services/api'
+import { convertToLab } from '@app/utils/converters'
 
 import { ModelName } from './ModelName'
 
@@ -24,7 +26,23 @@ export const LabsFeatureModel = types
       self.labs.clear()
     }
 
-    return { setLabs, clearLabs }
+    const uploadLabs = flow(function* () {
+      self.api.startLoading()
+
+      try {
+        const labs: Lab[] = yield scheduleLabsApiService.getScheduleLabs()
+
+        setLabs(labs.map(convertToLab))
+      } catch (e) {
+        const errorMessage = String(e)
+
+        self.api.setErrorMessage(errorMessage)
+      } finally {
+        self.api.finishLoading()
+      }
+    })
+
+    return { setLabs, clearLabs, uploadLabs }
   })
 
 export type LabsFeature = Instance<typeof LabsFeatureModel>
