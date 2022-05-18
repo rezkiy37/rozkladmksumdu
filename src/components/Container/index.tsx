753fa3b8ec163fff37ff0097b84ddaf0f6d966e8
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-19 00:49:47 +0300

    Create Container

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { useThemedStyles } from '@app/hooks/useThemedStyles'
+
+import { styles } from './styles'
+import { Props } from './types'
+
+const Container: React.FC<Props> = ({ style, children, ...props }) => {
+  const themedStyles = useThemedStyles(styles)
+
+  return (
+    <SafeAreaView
+      style={[themedStyles.container, style]}
+      {...props}
+    >
+      {children}
+    </SafeAreaView>
+  )
+}
+
+export default Container
---
+++ src/components/Container/types.ts
@@ -0,0 +1,3 @@
+import { SafeAreaViewProps } from 'react-native-safe-area-context'
+
+export type Props = SafeAreaViewProps & {}
