0f936247510c02c12c93ca81325a8dd5429403a9
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-19 00:49:10 +0300

    Extend sizes

--- src/utils/styles/sizes.ts
+++ src/utils/styles/sizes.ts
@@ -1,10 +1,12 @@
 import { Sizes } from '@app/types/Entities/Sizes'
 
 export const sizes: Sizes = {
+  zero: 0,
   sm: 8,
   md: 12,
   lg: 16,
   xl: 20,
+  logo: 96,
   '1/2': '50%',
   '1/3': '33.333%',
   '2/3': '66.666%',
@@ -16,4 +18,5 @@ export const sizes: Sizes = {
   '3/5': '60%',
   '4/5': '80%',
   full: '100%',
+  auto: 'auto',
 }
