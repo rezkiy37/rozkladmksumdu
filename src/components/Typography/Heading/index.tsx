8719384334aec6c3aa34ea7c8f2c2a68b0f8d764
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-18 23:53:53 +0300

    Integrate TypographyProps

--- src/components/Typography/Heading/index.tsx
+++ src/components/Typography/Heading/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Text } from 'react-native'
 
+import { TypographyProps } from '../types'
+
 import { useThemedStyles } from '@app/hooks/useThemedStyles'
 
 import { styles } from './styles'
 
-const Heading: React.FC = ({ children }) => {
+const Heading: React.FC<TypographyProps> = ({ children }) => {
   const themedStyles = useThemedStyles(styles)
 
   return <Text style={themedStyles.heading}>{children}</Text>
--- src/components/Typography/Paragraph/index.tsx
+++ src/components/Typography/Paragraph/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Text } from 'react-native'
 
+import { TypographyProps } from '../types'
+
 import { useThemedStyles } from '@app/hooks/useThemedStyles'
 
 import { styles } from './styles'
 
-const Paragraph: React.FC = ({ children }) => {
+const Paragraph: React.FC<TypographyProps> = ({ children }) => {
   const themedStyles = useThemedStyles(styles)
 
   return <Text style={themedStyles.paragraph}>{children}</Text>
