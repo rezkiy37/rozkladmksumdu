f1adeb9bb58eedae03aabfeccb5a0cb204f59b55
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-19 21:25:46 +0300

    Create AppVersion

--- src/components/AppVersion/index.tsx
+++ src/components/AppVersion/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
 import { Caption } from '@app/components/Typography'
+import { appVersionService } from '@app/services/appVersion/appVersionService'
 
-// TODO: Integrate dynamic app version getting
 // TODO: Use strings
-const appVersion: string = '0.1'
-
 export const AppVersion: React.FC = () => {
-  return <Caption>Version: {appVersion}</Caption>
+  return <Caption>Version: {appVersionService.getAppVersion()}</Caption>
 }
