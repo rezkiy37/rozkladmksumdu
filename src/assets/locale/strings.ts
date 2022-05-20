0df6be2b7ba84e8da1719f2ed20643b7edd8bf0e
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-20 22:23:57 +0300

    Integrate strings to linking

--- src/assets/locale/strings.ts
+++ src/assets/locale/strings.ts
@@ -4,6 +4,11 @@ import { Locale } from '@app/types/Entities/Locale'
 
 export const strings = new LocalizedStrings({
   [Locale.UA]: {
+    errors: {
+      linking: {
+        unavailableOpenLink: 'Unavailable to open the link',
+      },
+    },
     components: {
       appVersion: {
         title: 'Version: {0}',
