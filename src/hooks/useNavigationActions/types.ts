a6cdcddc73494b0cecaae4a5942e31177155c56c
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-05-21 00:24:42 +0300

    Create useNavigationActions hook

---
+++ src/hooks/useNavigationActions/types.ts
@@ -0,0 +1,5 @@
+import { NavigationProp } from '@react-navigation/native'
+
+import { HomeNavigationParams } from '@app/navigation/home/NavigationParams'
+
+export type NavigationParamList = NavigationProp<HomeNavigationParams>
