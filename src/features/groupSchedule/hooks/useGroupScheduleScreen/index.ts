9b1b23e8a3e2a10fa40b82988da1ebbe8a9a896a
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-06-05 23:50:04 +0300

    Create useGroupScheduleScreen

---
+++ src/features/groupSchedule/hooks/useGroupScheduleScreen/index.ts
@@ -0,0 +1,43 @@
+import { useCallback, useEffect } from 'react'
+
+import { RouteProp, useRoute } from '@react-navigation/native'
+
+import { useGroupScheduleFeature } from '../useGroupScheduleFeature'
+
+import { HomeNavigationParams } from '@app/navigation/home/NavigationParams'
+import { HomeScreenNames } from '@app/navigation/home/ScreenNames'
+
+// TODO: Figure the situation with unmounting out
+export const useGroupScheduleScreen = () => {
+  const {
+    params: { selectedGroup },
+  } = useRoute<RouteProp<HomeNavigationParams, HomeScreenNames.GroupSchedule>>()
+
+  const groupScheduleFeature = useGroupScheduleFeature()
+
+  const uploadGroupSchedules = useCallback(async () => {
+    await groupScheduleFeature.uploadGroupSchedules()
+  }, [groupScheduleFeature])
+
+  const clearScreenContent = useCallback(() => {
+    groupScheduleFeature.clearSelectedGroup()
+    groupScheduleFeature.clearScheduleType()
+    groupScheduleFeature.groupSchedule.clearDaysSchedule()
+  }, [groupScheduleFeature])
+
+  useEffect(() => {
+    groupScheduleFeature.setSelectedGroup(selectedGroup)
+
+    return () => {
+      clearScreenContent()
+    }
+  }, [groupScheduleFeature, selectedGroup, clearScreenContent])
+
+  useEffect(() => {
+    if (groupScheduleFeature.selectedGroup === null) {
+      return
+    }
+
+    uploadGroupSchedules()
+  }, [groupScheduleFeature.selectedGroup, uploadGroupSchedules])
+}
