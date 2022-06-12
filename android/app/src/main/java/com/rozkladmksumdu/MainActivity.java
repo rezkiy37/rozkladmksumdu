70d0538ea3932d0053e235ab0805eac32eab299f
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-06-12 19:20:18 +0300

    Integrate splash screen for Android

--- android/app/src/main/java/com/rozkladmksumdu/MainActivity.java
+++ android/app/src/main/java/com/rozkladmksumdu/MainActivity.java
@@ -6,6 +6,8 @@ import com.facebook.react.ReactRootView;
 
 import android.os.Bundle;
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -28,6 +30,8 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    
     super.onCreate(null);
   }
 
