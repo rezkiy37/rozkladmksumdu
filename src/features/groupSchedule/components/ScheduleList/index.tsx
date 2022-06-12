57f869a177f0287e70164504f4057e7087308c71
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-06-12 21:32:02 +0300

    Integrate Loader to ScheduleList

--- src/features/groupSchedule/components/ScheduleList/index.tsx
+++ src/features/groupSchedule/components/ScheduleList/index.tsx
@@ -6,6 +6,7 @@ import { observer } from 'mobx-react'
 import { useGroupScheduleFeature } from '../../hooks'
 import ScheduleCard from '../ScheduleCard'
 
+import Loader from '@app/components/Loader'
 import { useThemedStyles } from '@app/hooks/useThemedStyles'
 import { DayOfWeek } from '@app/types/entities/DayOfWeek'
 
@@ -14,7 +15,7 @@ import { styles } from './styles'
 const ScheduleList: React.FC = observer(() => {
   const themedStyles = useThemedStyles(styles)
 
-  const { dayOfWeekList } = useGroupScheduleFeature()
+  const { dayOfWeekList, api } = useGroupScheduleFeature()
 
   const renderScheduleCard = useCallback(
     ({ item }: { item: DayOfWeek }) => <ScheduleCard dayOfWeek={item} />,
@@ -22,6 +23,10 @@ const ScheduleList: React.FC = observer(() => {
   )
 
   const keyExtractor = useCallback((item: DayOfWeek) => item.toString(), [])
+
+  if (api.loading) {
+    return <Loader loading={api.loading} />
+  }
 
   return (
     <FlatList
