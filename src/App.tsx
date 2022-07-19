cd3edc24c021eaab6e4f69cbbdcd52fddad8e9c5
Mykhailo Kravchenko <rezkiy37@gmail.com>
2022-07-19 19:12:17 +0200

    Integrate raw notifications handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+// TODO: Remove ignoring the eslint
+/* eslint-disable no-console */
+import React, { useEffect } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import messaging, { firebase } from '@react-native-firebase/messaging'
 
 import { LocaleProvider } from '@app/contexts/LocaleContext'
 import { StoreProvider } from '@app/contexts/StoreContext'
@@ -11,8 +14,84 @@ import { store } from '@app/store'
 
 import { strings } from './assets/locale/strings'
 
+// TODO: Refactor
+const Notifications = async () => {
+  const defaultAppMessaging = firebase.messaging()
+
+  async function requestUserPermission() {
+    const authStatus = await defaultAppMessaging.requestPermission()
+
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL
+
+    if (enabled) {
+      console.log('requestUserPermission >> Authorization status:', authStatus)
+    }
+  }
+
+  const getToken = async () => {
+    try {
+      const token = await defaultAppMessaging.getToken()
+
+      console.log('getToken >> token >>', token)
+    } catch (error) {
+      console.log('getToken >> error >>', error)
+    }
+  }
+
+  defaultAppMessaging.onNotificationOpenedApp(remoteMessage => {
+    console.log(
+      'Notification caused app to open from background state:',
+      remoteMessage.notification,
+    )
+  })
+
+  // Check whether an initial notification is available
+  defaultAppMessaging.getInitialNotification().then(remoteMessage => {
+    if (remoteMessage) {
+      console.log(
+        'Notification caused app to open from quit state:',
+        remoteMessage.notification,
+      )
+    }
+  })
+
+  defaultAppMessaging.onMessage(async remoteMessage => {
+    console.log(
+      'onMessage >> notification of foreground state >> remoteMessage >>',
+      remoteMessage,
+    )
+  })
+
+  // Register background handler
+  defaultAppMessaging.setBackgroundMessageHandler(async remoteMessage => {
+    console.log(
+      'setBackgroundMessageHandler >> notification of foreground state >> remoteMessage >>',
+      remoteMessage,
+    )
+  })
+
+  return {
+    getFCMToken: getToken,
+    requestUserPermission,
+  }
+}
+
 const App: React.FC = () => {
   strings.setLanguage('ua')
+
+  useEffect(() => {
+    const launchNotifications = async () => {
+      const { requestUserPermission, getFCMToken } = await Notifications()
+
+      await requestUserPermission()
+
+      await getFCMToken()
+    }
+
+    launchNotifications()
+  }, [])
 
   return (
     <LocaleProvider>
