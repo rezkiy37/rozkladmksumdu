7e0882276e9c5bf69dac753b646cc8d11275c497
Michael Kravchenko <rezkiy37@gmail.com>
2023-02-09 15:06:39 +0100

    Implement google one tap

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {
+  GoogleOneTapSignIn,
+  statusCodes,
+} from 'react-native-google-one-tap-signin'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
@@ -13,6 +17,42 @@ import { strings } from './assets/locale/strings'
 
 const App: React.FC = () => {
   strings.setLanguage('ua')
+
+  useEffect(() => {
+    const signIn = async () => {
+      try {
+        const hasPlayServices = await GoogleOneTapSignIn.hasPlayServices()
+
+        // eslint-disable-next-line no-console
+        console.log('hasPlayServices >', hasPlayServices)
+
+        const userInfo = await GoogleOneTapSignIn.signIn()
+
+        // eslint-disable-next-line no-console
+        console.log('userInfo >', userInfo)
+      } catch (error: any) {
+        // eslint-disable-next-line no-console
+        console.log('error >', error)
+
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+          // user cancelled the login flow
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+          // operation (e.g. sign in) is in progress already
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+          // play services not available or outdated
+        } else {
+          // some other error happened
+        }
+      }
+    }
+
+    GoogleOneTapSignIn.configure({
+      webClientId:
+        '63337751862-n3j2rup5fevsdec4fepiahn3uqkigbnk.apps.googleusercontent.com',
+    })
+
+    signIn()
+  }, [])
 
   return (
     <LocaleProvider>
