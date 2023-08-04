abbde665d78dc0c92220390a8951604832b463e5
Davyd NRB <4661784+retyui@users.noreply.github.com>
2023-08-04 15:43:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import { AppRegistry } from 'react-native'
 import { name as appName } from './app.json'
 import App from './src/App'
 
-AppRegistry.registerComponent(appName, () => App); // 2
+AppRegistry.registerComponent(appName, () => App); // 22
